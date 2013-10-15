176d32c2b66a8a8c14d58fe794dbfe4cf6f47919
Sempl <maksonoff@gmail.com>
2013-10-15 03:11:30 +0300

    first comit

---
+++ 2.rb
@@ -0,0 +1,8 @@
+puts 'Kakovo Vashe imya?'
+name = gets.chomp
+puts 'Vashe otchestvo?'
+sec_name = gets.chomp
+puts 'A familiya'
+sernm = gets.chomp
+puts 'Rad privetstvovat\'vas, '+name+' '+sec_name+' '+sernm+' !'
+
