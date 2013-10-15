bf136e45b3de2b2a741f9e4233dafa1b8e62a776
Sempl <maksonoff@gmail.com>
2013-10-15 04:08:06 +0300

    Create 1.rb

---
+++ 1.rb
@@ -0,0 +1,20 @@
+seconds_in_year = 365*24*360
+puts 'skol\'ko sekynd prozhul chelovek?'
+seconds = gets.chomp
+norm_year = seconds.to_i/seconds_in_year
+time = Time.new
+year_now = time.year
+year_begin = year_now - norm_year
+days_plus=0
+while year_begin != year_now
+  if year_begin%4 == 0 and year_begin%100 != 0 
+    days_plus+=1
+  elsif year_begin%400 ==0
+    days_plus+=1
+  end
+ year_begin+=1
+end
+full_days = norm_year*365+days_plus
+full_year = full_days/365
+full_months = full_year*12
+puts 'etot chelovek prozhul ' +full_year.to_s+' let, ' +full_months.to_s+ ' mesyacev i ' +full_days.to_s+' dnei'
